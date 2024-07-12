e2cedb7a0d5684a9b013aac6854482da5c5ef829
tanu2719 <86526469+tanu2719@users.noreply.github.com>
2024-07-12 14:46:50 +0530

    Create multibranch-jenkinsfile1.groovy

---
+++ multibranch-jenkinsfile1.groovy
@@ -0,0 +1,14 @@
+pipeline{
+  agent any
+  environment
+  {
+    PIPE_LEVEL_VAR= "PIPELINE_LEVEL_VARIABLE"
+  }
+  stages{
+    stage('S-1'){
+      steps{
+        echo "printing ${PIPE_LEVEL_VAR}
+      }
+    }
+  }
+}
